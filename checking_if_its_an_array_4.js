5c785c8ce495fc7333ce6df7d8e5b5559364031c
Razib2019 <hafizurrahmanrazib@gmail.com>
2024-03-14 12:25:13 +0600

    first commit

---
+++ checking_if_its_an_array_4.js
@@ -0,0 +1,6 @@
+/*
+Instructions:
+    1. Create different variables, each containing either an array or a non-array value.
+    2. Now use isArray to check if each variable is an array.
+    3. Print a message to the console indicating whether each variable is an array or not.
+*/
---
+++ combining_arrays_5.js
@@ -0,0 +1,6 @@
+/*
+Instructions:
+    1. Create two arrays of your choice.
+    2. Use the concat method to combine the two arrays into a new array.
+    3. Print both the original arrays and the combined array using console.log().
+*/
